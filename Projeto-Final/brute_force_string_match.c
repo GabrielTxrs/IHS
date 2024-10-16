6c1cb4e35ddad38f918b23c8b93c1ed412da2b87
Gabriel Teixeira Silveira <gabriel.silveira.dev@gmail.com>
2024-10-16 20:51:47 -0300

    Correções de IO 	renamed:    Boyer_moore_string_match.c -> boyer_moore_string_match.c 	new file:   brute_force_string_match 	modified:   brute_force_string_match.c 	new file:   brute_force_string_match_O2 	new file:   brute_force_string_match_O3 	renamed:    shift_table_string_match.c -> horspool_string_match.c 	new file:   string.txt

--- Projeto-Final/brute_force_string_match.c
+++ Projeto-Final/brute_force_string_match.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void patternSearching(char *string, char *pattern){
+void patternSearching(char *string, char *pattern) {
     for (int i = 0; i < strlen(string); i++)
     {
         int count = 0;
@@ -19,20 +19,44 @@ void patternSearching(char *string, char *pattern){
         
         if (count == strlen(pattern))
         {
-            printf("Ditemukan pada index %d\n", i);
+            printf("Encontra-se na posição: %d\n", i);
         }   
     }
 }
 
-void main(){
-    char *string = malloc(sizeof(char)*100);
-    char *pattern = malloc(sizeof(char)*100);
+int main(int argc, char** argv){
 
-    printf("Input : ");
-    scanf(" %[^\n]%*c", string);
-    printf("Input Pola : ");
-    scanf(" %[^\n]%*c", pattern);
-    patternSearching(string, pattern);
-    // printf("\n%s", string);
+    FILE *file = fopen("string.txt", "r");
+    char *string;
+    long file_size;
 
+    if (file == NULL) {
+        fprintf(stderr, "Erro ao abrir o arquivo.\n");
+        return 1;
+    }
+
+    // Movendo o cursor para o final do arquivo para determinar o tamanho
+    fseek(file, 0, SEEK_END);
+    file_size = ftell(file);
+    rewind(file);
+
+    // Alocando memória dinamicamente para armazenar a string
+    string = (char *)malloc((file_size + 1) * sizeof(char));  // +1 para o caractere null-terminador
+    if (string == NULL) {
+        fprintf(stderr, "Erro de alocação de memória.\n");
+        fclose(file);
+        return 1;
+    }
+
+    // Lendo o conteúdo do arquivo no buffer
+    fread(string, sizeof(char), file_size, file);
+    string[file_size] = '\0';  // Adicionando o null-terminador
+
+    patternSearching(string, "ABCD");
+
+    // Liberando a memória e fechando o arquivo
+    free(string);
+    fclose(file);
+
+    return 0;
 }
